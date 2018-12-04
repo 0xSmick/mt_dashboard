0b42cc7a6d38703263bf20ef31e8d378cdc007af
Sheldon Smickley <sheldon.smickley@gmail.com>
2018-12-04 14:28:29 -0500

    add google login and logout

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from dash.dependencies import Input, Output
 from flask import Flask, session
 
 from dash_google_auth import GoogleOAuth
+from flask_dance.contrib.google import google
 
 # configure app
 server = Flask(__name__)
@@ -25,8 +26,9 @@ app.config['suppress_callback_exceptions']=True
 
 # configure google oauth using environment variables
 server.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
-server.config["GOOGLE_OAUTH_CLIENT_ID"] = os.environ["GOOGLE_OAUTH_CLIENT_ID"]
-server.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.environ["GOOGLE_OAUTH_CLIENT_SECRET"]
+
+server.config["GOOGLE_OAUTH_CLIENT_ID"] = os.environ['GOOGLE_CLIENT_ID_MT']
+server.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.environ['GOOGLE_SECRET_MT']
 
 # allow for insecure transport for local testing (remove in prod)
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
@@ -35,6 +37,7 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 authorized_emails = [
     "lucas.chapin@gmail.com",
     "1234@example.com",
+    "sheldonanalytics@gmail.com"
 ]
 
 auth = GoogleOAuth(
@@ -42,15 +45,33 @@ auth = GoogleOAuth(
     authorized_emails,
 )
 
-
 @server.route("/")
 def MyDashApp():
+    import pdb; pdb.set_trace();
     return app.index()
+
+@server.route("/logout")
+def logout():
+    resp = google.post(
+        'https://accounts.google.com/o/oauth2/revoke',
+        params={'token': session['google_oauth_token']['access_token']},
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+    )
+    session.clear()
+    return '''
+    <html>
+        <head>
+            <title>Logged Out</title>
+        </head>
+        <body>
+            <h1>You have been logged out</h1>
+        </body>
+    </html>'''
 
 
 app.layout = html.Div(children=[
     html.H1(children="Private Dash App"),
-
+    html.A("Logout", href="http://localhost:8050/logout"),
     html.Div(id='placeholder', style={'display':'none'}),
     html.Div(id='welcome'),
 
